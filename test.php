71093f15b95dc664d0b229c3e4327259332d1ab6
wangzhengyi <15866883396@126.com>
2013-04-10 10:43:50 +0800

    （1）根据type来区分是获取汉字拼音还是汉字首字母（2）完善测试用例

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Description:拼音转换类测试程序
+ */
+require_once dirname(__FILE__) . "/Pinyin.php";
+
+// 确定换行符
+$s_br = (php_sapi_name() == "cli") ? "\n" : "<br>";
+
+// 测试汉字转拼音
+$keyword = "黄老师很犀利，number1";
+$str = Pinyin::getPinyin($keyword);
+echo $str . $s_br;
+
+// 测试获取汉字首字母
+$keyword = "好联系";
+$str = Pinyin::getPinyin($keyword, 1);
+echo $str . $s_br;
